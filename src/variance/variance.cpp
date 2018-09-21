45a96dc75cf89cb06c21e1c079eeb94c995a6118
d00441522 <d00441522@huawei.com>
2018-09-21 03:57:52 -0400

    Fix gitignore and other

--- src/variance/variance.cpp
+++ src/variance/variance.cpp
@@ -5,12 +5,14 @@
 #include <numeric>
 #include <vector>
 
-Variance::Variance(decltype(cur_data) init_data) : Variance::Variance() {
+Variance::Variance(decltype(cur_data) init_data)
+    : Variance::Variance() {
   Variance::setCurData(init_data);
 }
-Variance::~Variance() {}
+Variance::~Variance() {
+}
 
-template <typename T>
+template<typename T>
 std::vector<T> operator+(const std::vector<T> &A, const std::vector<T> &B) {
   std::vector<T> AB;
   AB.reserve(A.size() + B.size());          // preallocate memory
@@ -19,16 +21,16 @@ std::vector<T> operator+(const std::vector<T> &A, const std::vector<T> &B) {
   return AB;
 }
 
-template <typename T>
+template<typename T>
 std::vector<T> &operator+=(std::vector<T> &A, const std::vector<T> &B) {
-  A.reserve(A.size() +
-            B.size());  // preallocate memory without erase original data
+  A.reserve(A.size() + B.size());  // preallocate memory without erase original data
   A.insert(A.end(), B.begin(), B.end());  // add B;
   return A;                               // here A could be named AB
 }
 
 void Variance::setCurData(decltype(cur_data) data) {
-  if (!this->cur_data.empty()) this->history += this->cur_data;
+  if (!this->cur_data.empty())
+    this->history += this->cur_data;
   this->cur_data = data;
   this->cur_mean = 0;
   this->cur_var = 0;
@@ -44,9 +46,9 @@ double calMean(std::vector<double> data) {
 double calVar(std::vector<double> data, double mean) {
   std::vector<double> diff(data.size());
   std::transform(data.begin(), data.end(), diff.begin(),
-                 [mean](double x) { return x - mean; });
-  return std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0) /
-         data.size();
+                 [mean](double x) {return x - mean;});
+  return std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0)
+      / data.size();
 }
 
 double Variance::calculate() {
@@ -54,15 +56,14 @@ double Variance::calculate() {
     this->cur_mean = calMean(this->cur_data);
     this->cur_var = calVar(this->cur_data, this->cur_mean);
     if (!this->history.empty()) {
-      this->mean = (this->history.size() * this->history_mean +
-                    this->cur_data.size() * this->cur_mean) /
-                   (this->history.size() + this->cur_data.size());
-      this->var =
-          (this->history.size() *
-               (this->history_var + pow(this->mean - this->history_mean, 2)) +
-           this->cur_data.size() *
-               (this->cur_var + pow(this->mean - this->cur_mean, 2))) /
-          (this->history.size() + this->cur_data.size());
+      this->mean = (this->history.size() * this->history_mean
+          + this->cur_data.size() * this->cur_mean)
+          / (this->history.size() + this->cur_data.size());
+      this->var = (this->history.size()
+          * (this->history_var + pow(this->mean - this->history_mean, 2))
+          + this->cur_data.size()
+              * (this->cur_var + pow(this->mean - this->cur_mean, 2)))
+          / (this->history.size() + this->cur_data.size());
     } else {
       this->mean = this->cur_mean;
       this->var = this->cur_var;
@@ -71,11 +72,15 @@ double Variance::calculate() {
   return this->var;
 }
 
-double Variance::getVariance() { return this->var; }
+double Variance::getVariance() {
+  return this->var;
+}
 
 void Variance::present() {
-  for (double v : this->cur_data) std::cout << v << " <- cur_data" << std::endl;
-  for (double v : this->history) std::cout << v << " <- history" << std::endl;
+  for (double v : this->cur_data)
+    std::cout << v << " <- cur_data" << std::endl;
+  for (double v : this->history)
+    std::cout << v << " <- history" << std::endl;
   std::cout << this->mean << " <- mean" << std::endl;
   std::cout << this->var << " <- var" << std::endl;
   std::cout << this->history_mean << " <- history_mean" << std::endl;
